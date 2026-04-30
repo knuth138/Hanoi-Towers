fa44e086897eb6f65618567d9e1cb62d8d8938cf
Tyler Knuth <knuth138@outlook.com>
2026-04-30 14:31:08 -0400

    Delete towers-of-hanoi directory

--- towers-of-hanoi/disc3.gd
+++
@@ -1,8 +0,0 @@
-extends "res://disc1.gd"
-
-func _ready():
-    free_texture   = load("res://art_assets/disc3_free.png")
-    linked_texture = load("res://art_assets/disc3_linked.png")
-    allowed_discs_under = [1, 2]
-    disc_offset = -4
-    disc_id = 3
